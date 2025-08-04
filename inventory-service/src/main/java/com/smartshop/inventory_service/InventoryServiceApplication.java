38a4afdffd7d791422f776ff4f2cba1cbc9ba9a5
Lakindu199854 <lakinduat@gmail.com>
2025-08-04 12:27:41 +0400

    Added the Eureka clients

--- inventory-service/src/main/java/com/smartshop/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/smartshop/inventory_service/InventoryServiceApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
 
 @SpringBootApplication
+
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
