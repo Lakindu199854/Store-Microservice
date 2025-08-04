7cab18e677776faa2e8708852a6404efb0a2d2de
Lakindu199854 <lakinduat@gmail.com>
2025-08-04 12:43:43 +0400

    Added client side load balancing-@LoadBalanced

--- order-service/src/main/java/com/smartshop/order_service/config/WebClientConfig.java
+++ order-service/src/main/java/com/smartshop/order_service/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.smartshop.order_service.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     //Here we have created a bean of the name webClient
+    //@LoadBalanced adds client side load balancing capabilities the webclient
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
     }
 }
